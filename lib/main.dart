6b86abc2c7662cdede783bc7234a2df4ba96ea47
Reuben Turner <groovinchip@gmail.com>
2021-06-25 11:58:07 -0400

    Rename file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:macosui_counter/platform_adapter.dart';
+import 'package:macosui_counter/platform_delegate.dart';
 
 void main() {
   runApp(
