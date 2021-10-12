047ab620b1f4d8c72bff7f7d186f513cf9a1199e
groovinchip <groovinchip@gmail.com>
2021-10-12 13:57:33 -0400

    feat: main entrypoints specific to desktop/mobile

---
+++ lib/main_macos.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:macosui_counter/apps/mac_app.dart';
+import 'package:macosui_counter/bloc/counter_bloc.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  final counterBloc = CounterBloc();
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<CounterBloc>.value(value: counterBloc),
+      ],
+      child: MacApp(),
+    ),
+  );
+}
